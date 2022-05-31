61e88c50f5970cf34b6689120c4bad2f15d33e0a
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-05-31 12:45:51 +0700

    style reset button

--- src/Hangman.js
+++ src/Hangman.js
@@ -86,7 +86,9 @@ class Hangman extends Component {
         <p className="Hangman-btns">
           {!gameOver ? this.generateButtons() : `You lose!`}
         </p>
-        <button onClick={this.reset}>Restart?</button>
+        <button id="reset" onClick={this.reset}>
+          Restart?
+        </button>
       </div>
     );
   }
